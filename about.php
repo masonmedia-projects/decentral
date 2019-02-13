a1e312e8f74e2002a163f5def532abe82a75c79c
Andrew Mason <andrewmasonmedia@gmail.com>
2019-02-13 17:13:58 -0500

    Updates to about, css

--- about.php
+++ about.php
@@ -169,14 +169,14 @@ include 'includes/header.php';
                 <h2 class="section-title knockout-gradient pb-4 pt-0" data-aos="fade" data-aos-duration="1000">The History of Decentral</h2>
                 <div class="timeline-item" data-aos="fade-up" data-aos-duration="1000">
                     <!--date-is='1' -->
-                    <h2 class="h5 orange">1</h2>
+                    <h2 class="h5 orange pb-2">1</h2>
                     <h3 class="h1">Early 2012</h3>
                     <p>Anthony Di Iorio organizes a variety of Bitcoin meetups in the Toronto area. BTC trades under $10. Anthony starts the Toronto Bitcoin Group.</p>
                     <hr class="mb-0 mt-4">
                 </div>
 
                 <div class="timeline-item" data-aos="fade-up" data-aos-duration="1000">
-                    <h2 class="h5 orange">2</h2>
+                    <h2 class="h5 orange pb-2">2</h2>
                     <h3 class="h1">Early 2013</h3>
                     <p>Anthony Di Iorio founds and is elected first Executive Director of the Bitcoin Alliance of Canada (now <a href="http://blockchainassociation.ca" target="_blank"><span class="orange">“Blockchain Association”</span>)</a></p>
                     <p>Kryptokit is conceived and development begins. Kryptokit becomes the first Bitcoin wallet browser extension on release.</p>
@@ -184,14 +184,14 @@ include 'includes/header.php';
                 </div>
 
                 <div class="timeline-item" data-aos="fade-up" data-aos-duration="1000">
-                    <h2 class="h5 orange">3</h2>
+                    <h2 class="h5 orange pb-2">3</h2>
                     <h3 class="h1">Late 2013</h3>
                     <p>Vitalik Buterin shows Anthony a project he has been working on: Anthony is the first reader of what will become the Ethereum Whitepaper. Anthony dedicates funds and his full attention to building a team to pursue the Ethereum Project and becomes co-founder of Ethereum.</p>
                     <p>Ethereum is born: Decentral gives the Ethereum Project its first home. Anthony Di Iorio, Mihai Alisie, Vitalik Buterin, Charles Hoskinson and Amir Chetrit co-found the project to develop the platform laid out in the whitepaper. (Joseph Lubin, Gavin Wood, and Jeffrey Wilke are added as founders a few months later).</p>
                     <hr class="mb-0 mt-4">
                 </div>
                 <div class="timeline-item" data-aos="fade-up" data-aos-duration="1000">
-                    <h2 class="h5 orange">4</h2>
+                    <h2 class="h5 orange pb-2">4</h2>
                     <h3 class="h1">Early 2014</h3>
                     <p>New Year’s Day: Decentral opens its doors at 64 Spadina Ave. The Bitcoin ATM in this new office is the first in Toronto and second in the country.</p>
                     <p>Decentral organizes and runs the Bitcoin Expo 2014 (bitcoinexpo.ca), attended by thousands and the biggest cryptocurrency event in Canada at the time.</p>
@@ -199,7 +199,7 @@ include 'includes/header.php';
                 </div>
 
                 <div class="timeline-item" data-aos="fade-up" data-aos-duration="1000">
-                    <h2 class="h5 orange">5</h2>
+                    <h2 class="h5 orange pb-2">5</h2>
                     <h3 class="h1">Summer 2014</h3>
                     <p>Ethereum global crowdsale begins and eventually leads to $18m in ether sales (now worth over $44 billion).</p>
                     <p>Rushwallet by KryptoKit launches in beta as a simple HTML5 Bitcoin wallet.</p>
@@ -207,28 +207,28 @@ include 'includes/header.php';
                 </div>
 
                 <div class="timeline-item" data-aos="fade-up" data-aos-duration="1000">
-                    <h2 class="h5 orange">6</h2>
+                    <h2 class="h5 orange pb-2">6</h2>
                     <h3 class="h1">Late 2014</h3>
                     <p>Decentral hosts its 100th event. Seeks new event space to accommodate demand and crowd size.</p>
                     <hr class="mb-0 mt-4">
                 </div>
 
                 <div class="timeline-item" data-aos="fade-up" data-aos-duration="1000">
-                    <h2 class="h5 orange">7</h2>
+                    <h2 class="h5 orange pb-2">7</h2>
                     <h3 class="h1">Early 2015</h3>
                     <p>Decentral partners with <a href="https://www.marsdd.com/facilities/event-space/" target="_blank"><span class="orange">MaRS</span></a> to host events in 2015.</p>
                     <hr class="mb-0 mt-4">
                 </div>
 
                 <div class="timeline-item" data-aos="fade-up" data-aos-duration="1000">
-                    <h2 class="h5 orange">8</h2>
+                    <h2 class="h5 orange pb-2">8</h2>
                     <h3 class="h1">Early 2016</h3>
                     <p>Jaxx 1.0 released. Additional platforms supported.</p>
                     <hr class="mb-0 mt-4">
                 </div>
 
                 <div class="timeline-item" data-aos="fade-up" data-aos-duration="1000">
-                    <h2 class="h5 orange">9</h2>
+                    <h2 class="h5 orange pb-2">9</h2>
                     <h3 class="h1">Late 2016</h3>
                     <p>Jaxx passes 100,000 users; Android is the most popular of the 9 platforms Jaxx supports.</p>
                     <p>In-app cryptocurrency swapping features go live; Jaxx users are able to exchange a variety of cryptocurrencies for each other, instantly.</p>
@@ -236,28 +236,28 @@ include 'includes/header.php';
                 </div>
 
                 <div class="timeline-item" data-aos="fade-up" data-aos-duration="1000">
-                    <h2 class="h5 orange">10</h2>
+                    <h2 class="h5 orange pb-2">10</h2>
                     <h3 class="h1">Early 2017</h3>
                     <p>Jaxx hits 300,000 users; users can pair their Jaxx masterseeds across iOS, Android, Chrome Browser, Linux, Mac, and Windows.</p>
                     <hr class="mb-0 mt-4">
                 </div>
 
                 <div class="timeline-item" data-aos="fade-up" data-aos-duration="1000">
-                    <h2 class="h5 orange">11</h2>
+                    <h2 class="h5 orange pb-2">11</h2>
                     <h3 class="h1">Spring 2017</h3>
                     <p>Decentral announces upwards of 70 industry partnerships; companies include Coinbase, RSK, BitPay, Bittrex, Blockchain Capital, QTUM, Bancor, MonaCoin, Omni, Lisk, Stratus and others.</p>
                     <hr class="mb-0 mt-4">
                 </div>
 
                 <div class="timeline-item" data-aos="fade-up" data-aos-duration="1000">
-                    <h2 class="h5 orange">12</h2>
+                    <h2 class="h5 orange pb-2">12</h2>
                     <h3 class="h1">Summer 2017</h3>
                     <p>Decentral Grand Odyssey Boat Cruise - hit blockchain event celebrates 4 years of Decentral events.</p>
                     <hr class="mb-0 mt-4">
                 </div>
 
                 <div class="timeline-item" data-aos="fade-up" data-aos-duration="1000">
-                    <h2 class="h5 orange">13</h2>
+                    <h2 class="h5 orange pb-2">13</h2>
                     <h3 class="h1">November 2017</h3>
                     <p>Jaxx exceeds 400,000 users around the world.</p>
                     <p>Decentral Inc. begins construction for 5-floor, 15,000 square foot office and event space in the heart Toronto’s financial district.</p>
@@ -265,7 +265,7 @@ include 'includes/header.php';
                 </div>
 
                 <div class="timeline-item" data-aos="fade-up" data-aos-duration="1000">
-                    <h2 class="h5 orange">14</h2>
+                    <h2 class="h5 orange pb-2">14</h2>
                     <h3 class="h1">December 2017</h3>
                     <p><p>Bitcoin prices hits $20,000 CAD. 2000x higher than when Decentral’s founder, Anthony Di Iorio, started building the Toronto cryptocurrency community.</p>
                     <hr class="mb-0 mt-4">
