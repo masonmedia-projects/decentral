e312d53b0051114236b5775a7e2010775b432525
Andrew Mason <andrewmasonmedia@gmail.com>
2019-03-13 09:47:03 -0400

    Update to schema data header

--- includes/header.php
+++ includes/header.php
@@ -44,10 +44,10 @@
     <link rel="stylesheet" href="/assets/css/normalize.css">
     <link rel="stylesheet" href="/assets/css/font-awesome-4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="/assets/css/bootstrap.min.css">
-    <link rel="stylesheet" href="/assets/css/main.min.css?=10">
+    <link rel="stylesheet" href="/assets/css/main.min.css?=011">
     <!--schema structured data-->
     <script type="application/ld+json">
-         {
+    {
           "@context": "https://schema.org",
             "@type": "Organization",
               "name": "Decentral Inc.",
